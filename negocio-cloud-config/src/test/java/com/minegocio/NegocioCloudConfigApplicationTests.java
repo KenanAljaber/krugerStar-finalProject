d759b5ce01936b78b09f7afce9ecece279e2e10e
kenoisplaying <keno12333@hotmail.com>
2023-01-08 10:05:14 -0500

    adding the clou server configuration

---
+++ negocio-cloud-config/src/test/java/com/minegocio/NegocioCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minegocio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NegocioCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
